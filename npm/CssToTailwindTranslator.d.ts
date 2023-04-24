20a2a1136625e74e6f244d72b546b5a6a4af6251
xm <1749207188@qq.com>
2023-04-24 22:12:51 +0800

    del npm dist

--- npm/CssToTailwindTranslator.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ResultCode {
-    selectorName: string;
-    resultVal: string;
-}
-export declare const specialAttribute: string[];
-export declare const CssToTailwindTranslator: (code: string) => {
-    code: 'SyntaxError' | 'OK';
-    data: ResultCode[];
-};
