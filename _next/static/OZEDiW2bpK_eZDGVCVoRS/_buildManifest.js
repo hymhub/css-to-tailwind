0d6fcc77f51b7e1afe60945ca9be9bbd373a440c
hymhub <hymhub@users.noreply.github.com>
2023-04-24 13:42:21 +0000

    deploy: 42d57cad358b1f5565d693c9fa71ddd9a5453446

---
+++ _next/static/OZEDiW2bpK_eZDGVCVoRS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/115-bd22b533a8ca4e70.js","static/chunks/pages/index-1359d32e405a9889.js"],"/_error":["static/chunks/pages/_error-6ae1767c9313c80e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
