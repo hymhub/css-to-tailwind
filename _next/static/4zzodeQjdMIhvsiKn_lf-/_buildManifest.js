68100f463b57085a1da429191a1b928deacb12a5
hymhub <hymhub@users.noreply.github.com>
2023-08-06 08:48:46 +0000

    deploy: 091994112212861a47d52de4fff6bd42d459eb9e

---
+++ _next/static/4zzodeQjdMIhvsiKn_lf-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/18-c2040f5d6eb48268.js","static/chunks/pages/index-04013829ce86e7aa.js"],"/_error":["static/chunks/pages/_error-f7977824d9b305ed.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
